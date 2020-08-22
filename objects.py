7f7752674efa50f9b7def78c9fcc03b9765eece8
Kristi Wees <brandonwees@gmail.com>
2020-08-22 07:18:24 -0400

    created Assignment objects

---
+++ objects.py
@@ -0,0 +1,25 @@
+from uuid import uuid1
+from enum import Enum
+
+class Status:
+    notStarted = 0
+    inProgress = 1
+    complete = 2
+
+class Assignment:
+    def __init__(self, assignmentName, className, typeName, dueDate, notes, duration, attachments = [], status=Status.notStarted, uuid=uuid1()):
+        self.assignmentName = assignmentName
+        self.className = className
+        self.typeName = typeName
+        self.dueDate = dueDate
+        self.attachments = attachments
+        self.notes = notes
+        self.duration = duration
+
+        self.uuid = uuid.hex
+
+    def __eq__(self, other):
+        return self.uuid == other.uuid
+
+    def saveAttachment(self, file):
+        file.save("/attachments/" + self.uuid + "/"+ uuid1().hex)
