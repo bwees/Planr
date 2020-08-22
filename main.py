c7c25cf0888766acc82963282db4bc23d844b005
bwees <brandonwees@gmail.com>
2020-08-22 15:39:23 -0400

    stuff

--- main.py
+++ main.py
@@ -19,6 +19,18 @@ def calcRings(totalTime,activityTime,workTime):
         return [workTime,activityTime,totalTime-activityTime-workTime], ["Work Time", "Activity Time", "Free Time"]
 
 
+def htmlString(tags):
+    htmlString = ""
+    for tag in tags:
+        if tag == "Work Time":
+            htmlString += '<div class="chart-note mr-0 d-block"><span class="dot dot--blue"></span><span>Work Time</span></div>'
+        if tag == "Activity Time":
+            htmlString+= '<div class="chart-note mr-0 d-block"><span class="dot dot--red"></span><span>Activity Time</span></div>'
+        if tag == "Free Time":
+            htmlString+= '<div class="chart-note mr-0 d-block"><span class="dot dot--green"></span><span>Free Time</span></div>'
+    return htmlString
+
+
 @app.route('/')
 def index():
 
@@ -55,16 +67,7 @@ def newAssignment():
     else:
         return render_template("add_assignment.html")
 
-def htmlString(tags):
-    htmlString = ""
-    for tag in tags:
-        if tag == "Work Time":
-            htmlString += '<div class="chart-note mr-0 d-block"><span class="dot dot--blue"></span><span>Work Time</span></div>'
-        if tag == "Activity Time":
-            htmlString+= '<div class="chart-note mr-0 d-block"><span class="dot dot--red"></span><span>Activity Time</span></div>'
-        if tag == "Free Time":
-            htmlString+= '<div class="chart-note mr-0 d-block"><span class="dot dot--green"></span><span>Free Time</span></div>'
-    return htmlString
+
 
 if __name__ == "__main__":
     app.run()
