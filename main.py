2297e7564536696d2e0658eaab0872e9e9d6daa8
Kristi Wees <brandonwees@gmail.com>
2020-08-22 09:34:14 -0400

    added tinydb

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
+from tinydb import TinyDB, Query
+
+db = TinyDB('planr.json')
+
 app = Flask(__name__, template_folder="web")
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
