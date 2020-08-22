378bdfefd8430b32b53ecedce86340fbf443c3a0
bwees <brandonwees@gmail.com>
2020-08-22 10:25:49 -0400

    update for new structure

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tinydb import TinyDB, Query
 
 db = TinyDB('planr.json')
 
-app = Flask(__name__, template_folder="web/", static_folder="static/")
+app = Flask(__name__, template_folder="web/", static_folder="web/static/")
 
 @app.route('/')
 def hello_world():
